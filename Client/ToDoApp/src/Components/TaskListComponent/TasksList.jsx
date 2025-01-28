5f7e2527b5a44cf7b8599197bb8d0117306454a1
Winklaa22 <49796412+Winklaa22@users.noreply.github.com>
2025-01-28 14:57:42 +0100

    Table rows animations

--- Client/ToDoApp/src/Components/TaskListComponent/TasksList.jsx
+++ Client/ToDoApp/src/Components/TaskListComponent/TasksList.jsx
@@ -1,19 +1,25 @@
 import {useState } from 'react';
 import axios from 'axios';
 import "./TaskList.css"
+import { motion, AnimatePresence } from "framer-motion";
+
 
 const TasksList = ({fetchTasksFunc, tasksList, setEditingTaskFunc}) =>{
 
   const [isCompleted, setIsCompleted] = useState(false);
 
-    const deleteTask = async (taskGuid) => {
-        try {
-          await axios.delete(`http://localhost:5000/Tasks/DeleteTask?taskGuid=${taskGuid}`);
-          fetchTasksFunc();
-        } catch (error) {
-          console.error('Error deleting task', error);
-        }
-      };
+  const animDuration = .2
+
+  const deleteTask = async (taskGuid) => {
+    setTimeout(async () => {
+      try {
+        await axios.delete(`http://localhost:5000/Tasks/DeleteTask?taskGuid=${taskGuid}`);
+        fetchTasksFunc();
+      } catch (error) {
+        console.error("Error deleting task", error);
+      }
+    }, (animDuration * 1000));
+  };
     
       const completedChange = async (taskGuid, isCompleted) =>{
         setIsCompleted(!setIsCompleted);
@@ -42,29 +48,38 @@ const TasksList = ({fetchTasksFunc, tasksList, setEditingTaskFunc}) =>{
               </tr>
             </thead>
             <tbody>
-              {tasksList.map((task) => (
-                <tr className={task.isCompleted ? 'task-checked' : "task-unchecked"} key={task.taskGuid}>
-                  <td>{task.taskName}</td>
-                  <td>{task.taskDesc ? task.taskDesc : "No description"}</td>
-                  <td>{new Date(task.createOn).toISOString().split('T')[0]}</td>
-                  <td>
-                    <div className='isCompleted-label'>
-                      <input type="checkbox" 
-                        checked={task.isCompleted} 
-                        onChange={e => completedChange(task.taskGuid, e.target.checked)}
-                      />
-                    </div>
-                  </td>
-                  <td>
-                    <div className='actions-btns'>
-                      <button className="edit-btn" onClick={() => setEditingTaskFunc(task)}>Edit</button>
-                      <button className="delete-btn" onClick={() => deleteTask(task.taskGuid)}>
-                        Delete
-                      </button>                      
-                    </div>
-                  </td>
-                </tr>
-              ))}
+              <AnimatePresence>
+                {tasksList.map((task) => (
+                  <motion.tr
+                    className={task.isCompleted ? "task-checked" : "task-unchecked"}
+                    key={task.taskGuid}
+                    initial={{ opacity: 0}}
+                    animate={{ opacity: 1}}
+                    exit={{ opacity: 0}}
+                    transition={{ duration: animDuration }}
+                  >
+                    <td>{task.taskName}</td>
+                    <td>{task.taskDesc ? task.taskDesc : "No description"}</td>
+                    <td>{new Date(task.createOn).toISOString().split('T')[0]}</td>
+                    <td>
+                      <div className='isCompleted-label'>
+                        <input type="checkbox" 
+                          checked={task.isCompleted} 
+                          onChange={e => completedChange(task.taskGuid, e.target.checked)}
+                        />
+                      </div>
+                    </td>
+                    <td>
+                      <div className='actions-btns'>
+                        <button className="edit-btn" onClick={() => setEditingTaskFunc(task)}>Edit</button>
+                        <button className="delete-btn" onClick={() => deleteTask(task.taskGuid)}>
+                          Delete
+                        </button>                      
+                      </div>
+                    </td>
+                  </motion.tr>
+                ))}
+              </AnimatePresence>
             </tbody>
           </table>
         )}
